a7b7252cf3b4d0b0d4ffbf890ec16d90176554f8
juliac-jpg <juliafcostcds@gmail.com>
2026-04-07 19:41:11 -0300

    commit inicial

---
+++ src/main/java/org/example/streamingmusical/StreamingMusicalApplication.java
@@ -0,0 +1,13 @@
+package org.example.streamingmusical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingMusicalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamingMusicalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/streamingmusical/StreamingMusicalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.streamingmusical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingMusicalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
