3e5d44f36e6136d3861f6c59709a7f10c1f526ee
juliac-jpg <juliafcostcds@gmail.com>
2026-04-07 21:01:04 -0300

    create service

---
+++ src/main/java/org/example/streamingmusical/service/MusicaService.java
@@ -0,0 +1,43 @@
+package org.example.streamingmusical.service;
+
+import org.example.streamingmusical.model.Musica;
+import org.example.streamingmusical.repository.MusicaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MusicaService {
+
+    private final MusicaRepository repository;
+
+    public MusicaService(MusicaRepository repository){
+        this.repository = repository;
+    }
+
+    public List<Musica> listarMusicas(){
+        return repository.findAll();
+    }
+
+    public Musica buscarId(Long id){
+        return repository.findById(id).orElseThrow(() -> new RuntimeException("Música não encontrada."));
+    }
+
+    public List<Musica> buscarMusica(String nome){
+        return repository.findByContaingNomeIgnoreCase(nome);
+    }
+
+    public Musica salvarMusica(Musica musica){
+        return repository.save(musica);
+    }
+
+    public Musica alterarMusica(Long id, Musica musica){
+        buscarId(id);
+        musica.setId(id);
+        return repository.save(musica);
+    }
+
+    public void deletarMusica(Long id){
+        repository.deleteById(id);
+    }
+}
